89ba2fbb28af4d0cf2dca381a25bf8bfcc079aa1
Andrea Baldan <andrea@duffel.com>
2022-07-09 22:35:38 +0100

    Add first basic unit tests for client

---
+++ test/client_test.exs
@@ -0,0 +1,16 @@
+defmodule Tweexir.ClientTest do
+  use ExUnit.Case
+  alias Tweexir.Client
+
+  describe "process_request_url/1" do
+    test "returns the joined URI starting with https://api.twitter.com/2/" do
+      assert Client.process_request_url("/foo") == "https://api.twitter.com/2/foo"
+      assert Client.process_request_url("/foo/bar") == "https://api.twitter.com/2/foo/bar"
+    end
+
+    test "returns the joined URI starting with https://api.twitter.com/2/ with a non-leading / relative path" do
+      assert Client.process_request_url("foo") == "https://api.twitter.com/2/foo"
+      assert Client.process_request_url("foo/bar") == "https://api.twitter.com/2/foo/bar"
+    end
+  end
+end
