4e32a58bfef8ca7bfcbb2a1c4a3ddba4c47d63a8
Andrea Baldan <andrea@duffel.com>
2022-10-29 00:46:22 +0100

    Handle async responses

--- lib/tweexir/http/client.ex
+++ lib/tweexir/http/client.ex
@@ -5,19 +5,20 @@ defmodule Tweexir.Http.Client do
   def get(url, headers \\ [], opts \\ []) do
     url
     |> Base.get(headers, opts)
-    |> handle_error()
+    |> handle_response()
   end
 
   def post(url, body, headers \\ [], opts \\ []) do
     url
     |> Base.post(body, headers, opts)
-    |> handle_error()
+    |> handle_response()
   end
 
-  defp handle_error({:ok, %HTTPoison.Response{status_code: status_code, body: body}})
+  defp handle_response({:ok, %HTTPoison.Response{status_code: status_code, body: body}})
        when status_code in 200..299,
        do: {:ok, body}
 
-  defp handle_error({:ok, %HTTPoison.Response{body: body}}), do: {:http_error, body}
-  defp handle_error({:error, %HTTPoison.Error{reason: reason}}), do: {:error, reason}
+  defp handle_response({:ok, %HTTPoison.Response{body: body}}), do: {:http_error, body}
+  defp handle_response({:error, %HTTPoison.Error{reason: reason}}), do: {:error, reason}
+  defp handle_response(response), do: response
 end
